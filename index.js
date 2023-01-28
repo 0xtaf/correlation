4ac39f6c9c39155c85ac405b7bdb70d2af091d01
Tayfun Sur <tayfunsur@gmail.com>
2023-01-28 21:23:12 +0300

    Move helpers to outside

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 require("dotenv").config({ path: ".env" });
+const { tokens, correlatedPairs } = require("./tokens");
 const axios = require("axios");
 
 const TelegramBot = require("node-telegram-bot-api");
@@ -13,73 +14,6 @@ function wait(delay) {
   });
 }
 
-const tokens = [
-  "HOOKUSDT",
-  "MAGICUSDT",
-  "HFTUSDT",
-  "SANTOSUSDT",
-  "LAZIOUSDT",
-  "PORTOUSDT",
-  "ALPINEUSDT",
-  "ATMUSDT",
-  "ASRUSDT",
-  "ACMUSDT",
-  "JUVUSDT",
-  "BARUSDT",
-  "CITYUSDT",
-  "PSGUSDT",
-  "OGUSDT",
-  "AGIXBUSD",
-  "FETBUSD",
-  "CTXCBUSD",
-  "VOXELUSDT",
-  "LOKAUSDT",
-  "BONDUSDT",
-  "WINGUSDT",
-  "SNMBUSD",
-  "VIBBUSD",
-  "SUNUSDT",
-  "JSTUSDT",
-  "TLMUSDT",
-  "TVKUSDT",
-  "AXSUSDT",
-  "SLPUSDT",
-];
-
-const correlatedPairs = {
-  HOOKUSDT: "MAGICUSDT, HFTUSDT",
-  MAGICUSDT: "HOOKUSDT, HFTUSDT",
-  HFTUSDT: "HOOKUSDT, MAGICUSDT",
-  SANTOSUSDT: "LAZIOUSDT PORTOUSDT ALPINEUSDT",
-  LAZIOUSDT: "SANTOSUSDT PORTOUSDT ALPINEUSDT",
-  PORTOUSDT: "SANTOSUSDT LAZIOUSDT ALPINEUSDT",
-  ALPINEUSDT: "SANTOSUSDT LAZIOUSDT PORTOUSDT",
-  ATMUSDT: "ASRUSDT ACMUSDT JUVUSDT BARUSDT CITYUSDT PSGUSDT OGUSDT",
-  ASRUSDT: "ATMUSDT ACMUSDT JUVUSDT BARUSDT CITYUSDT PSGUSDT OGUSDT",
-  ACMUSDT: "ATMUSDT ASRUSDT JUVUSDT BARUSDT CITYUSDT PSGUSDT OGUSDT",
-  JUVUSDT: "ATMUSDT ASRUSDT ACMUSDT BARUSDT CITYUSDT PSGUSDT OGUSDT",
-  BARUSDT: "ATMUSDT ASRUSDT ACMUSDT JUVUSDT CITYUSDT PSGUSDT OGUSDT",
-  CITYUSDT: "ATMUSDT ASRUSDT ACMUSDT JUVUSDT BARUSDT PSGUSDT OGUSDT",
-  PSGUSDT: "ATMUSDT ASRUSDT ACMUSDT JUVUSDT BARUSDT CITYUSDT OGUSDT",
-  OGUSDT: "ATMUSDT ASRUSDT ACMUSDT JUVUSDT BARUSDT CITYUSDT PSGUSDT",
-  AGIXBUSD: "FETBUSD CTXCBUSD",
-  FETBUSD: "AGIXBUSD CTXCBUSD",
-  CTXCBUSD: "AGIXBUSD FETBUSD",
-  VOXELUSDT: "LOKAUSDT",
-  LOKAUSDT: "VOXELUSDT",
-  BONDUSDT: "WINGUSDT",
-  WINGUSDT: "BONDUSDT",
-  SNMBUSD: "VIBBUSD",
-  VIBBUSD: "SNMBUSD",
-  SUNUSDT: "JSTUSDT",
-  JSTUSDT: "SUNUSDT",
-  TLMUSDT: "TVKUSDT",
-  TVKUSDT: "TLMUSDT",
-  AXSUSDT: "SLPUSDT",
-  SLPUSDT: "AXSUSDT",
-  AVAXUSDT: "JOEUSDT",
-};
-
 let url = "https://api.binance.com/api/v3/ticker?&windowSize=5m&symbols=%5B";
 
 tokens.forEach((token, index) => {
@@ -92,20 +26,24 @@ tokens.forEach((token, index) => {
 
 url = url + "%5D";
 
+let result = "";
 const fetchAll = () => {
   axios.get(url).then(response => {
-    const data = response.data.filter(data => data.priceChangePercent > 4);
+    const data = response.data.filter(data => data.priceChangePercent > 0.4);
 
-    const beautified = data.map(data => {
-      return {
-        symbol: data.symbol,
-        change: data.priceChangePercent,
-        korelesi: correlatedPairs[`${data.symbol}`],
-      };
+    data.forEach(data => {
+      result =
+        result +
+        `
+${data.symbol}
+Artis: %${Number(data.priceChangePercent).toFixed(1)}
+Korele pairler: ${correlatedPairs[`${data.symbol}`]}
+
+`;
     });
 
-    if (beautified.length) {
-      bot.sendMessage("-576436107", `test: ${JSON.stringify(beautified)}`);
+    if (data.length) {
+      bot.sendMessage("-576436107", result);
     }
   });
 };
